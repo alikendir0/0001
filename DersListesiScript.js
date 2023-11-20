8c055c151a2242d943edfb25fdb8e4cb43224a6d
Ali Kendir <alikendir2002@gmail.com>
2023-11-21 00:33:50 +0300

    class-assignment implementation

--- DersListesiScript.js
+++ DersListesiScript.js
@@ -1,5 +1,6 @@
 window.onload = function () {
   modalButtonProperties();
+  loadTableContents();
   checkTableData();
 };
 
@@ -56,85 +57,160 @@ function clearInputFields() {
   });
 }
 
+function deleteClass(classRow) {
+  classRow.reverse();
+  const container = document.querySelector(".container");
+  for (let i = 0; i < classRow.length; i++) {
+    if (classRow[i] >= 1 && classRow[i] < container.rows.length) {
+      container.deleteRow(classRow[i]);
+    }
+  }
+  checkTableData();
+}
+
+function saveTableContents() {
+  const container = document.querySelector(".container");
+  const contents = Array.from(container.rows).map((row) =>
+    Array.from(row.cells).map((cell) => cell.innerHTML)
+  );
+  const contentsJson = JSON.stringify(contents);
+  localStorage.setItem("classtableContents", contentsJson);
+  loadTableContents();
+}
+
+function loadTableContents() {
+  const container = document.querySelector(".container");
+  const contentsJson = localStorage.getItem("classtableContents");
+  if (contentsJson) {
+    const contents = JSON.parse(contentsJson);
+    contents.forEach((rowContents, rowIndex) => {
+      let row = container.rows[rowIndex];
+      if (!row) {
+        row = container.insertRow(rowIndex);
+      }
+      rowContents.forEach((cellContents, cellIndex) => {
+        let cell = row.cells[cellIndex];
+        if (!cell) {
+          cell = row.insertCell(cellIndex);
+        }
+        cell.innerHTML = cellContents;
+      });
+    });
+  }
+}
+
 function submit() {
-  let temp = true;
   let ders = document.getElementById("ders").value;
-  while (temp) {
-    if (!(ders === null || ders === "")) {
-      temp = false;
-    } else {
-      hataliAppear();
-      return;
-    }
-  }
-
-  temp = true;
   let fakulte = document.getElementById("fakulte").value;
-  while (temp) {
-    if (!(fakulte === null || fakulte === "")) {
-      temp = false;
-    } else {
-      hataliAppear();
-      return;
-    }
-  }
-
-  temp = true;
   let zaman = document.getElementById("zaman").value;
-  while (temp) {
-    if (!(zaman === null || zaman === "")) {
-      temp = false;
-    } else {
-      hataliAppear();
-      return;
-    }
-  }
-
-  temp = true;
   let sinif = document.getElementById("sinif").value;
-  while (temp) {
-    if (!(sinif === null || sinif === "")) {
-      temp = false;
-    } else {
-      hataliAppear();
-      return;
-    }
-  }
-
-  temp = true;
   let ogretici = document.getElementById("ogretici").value;
-  while (temp) {
-    if (!(ogretici === null || ogretici === "")) {
-      temp = false;
-    } else {
-      hataliAppear();
-      return;
-    }
+  const modal = document.getElementById("modal");
+  const overlay = document.getElementById("overlay");
+  const container = document.querySelector(".container");
+
+  if (!credentialsCheck(ders, fakulte, zaman, sinif, ogretici)) {
+    return;
   }
 
+  createNewRow(container, ders, fakulte, zaman, sinif, ogretici);
+  clearInputFields();
+  modalDisappear(modal, overlay);
+  checkTableData();
+}
+
+function createNewRow(container, ders, fakulte, zaman, sinif, ogretici) {
   const newRow = document.createElement("tr");
+  const checkbox = document.createElement("input");
+  const boxCell = document.createElement("td");
   const dersCell = document.createElement("td");
   const fakulteCell = document.createElement("td");
   const zamanCell = document.createElement("td");
   const sinifCell = document.createElement("td");
   const ogreticiCell = document.createElement("td");
+  checkbox.className = "Checkbox";
+  checkbox.type = "checkbox";
   dersCell.textContent = ders;
   fakulteCell.textContent = fakulte;
   zamanCell.textContent = zaman;
   sinifCell.textContent = sinif;
   ogreticiCell.textContent = ogretici;
+  newRow.appendChild(boxCell);
+  boxCell.appendChild(checkbox);
   newRow.appendChild(dersCell);
   newRow.appendChild(fakulteCell);
   newRow.appendChild(zamanCell);
   newRow.appendChild(sinifCell);
   newRow.appendChild(ogreticiCell);
 
-  const container = document.querySelector(".container");
   container.appendChild(newRow);
+}
 
-  const modal = document.getElementById("modal");
-  const overlay = document.getElementById("overlay");
-  clearInputFields();
-  modalDisappear(modal, overlay);
-  checkTableData();
+function getCheckedPositions() {
+  const checkboxes = document.querySelectorAll(".Checkbox");
+  const checkedPositions = [];
+  checkboxes.forEach((checkbox, index) => {
+    if (checkbox.checked) {
+      checkedPositions.push(index + 1);
+    }
+  });
+  return checkedPositions;
+}
+
+function credentialsCheck(ders, fakulte, zaman, sinif, ogretici) {
+  let temp = true;
+
+  while (temp) {
+    if (!(ders === null || ders === "")) {
+      temp = false;
+    } else {
+      hataliAppear();
+      return false;
+    }
+  }
+
+  temp = true;
+
+  while (temp) {
+    if (!(fakulte === null || fakulte === "")) {
+      temp = false;
+    } else {
+      hataliAppear();
+      return false;
+    }
+  }
+
+  temp = true;
+
+  while (temp) {
+    if (!(zaman === null || zaman === "")) {
+      temp = false;
+    } else {
+      hataliAppear();
+      return false;
+    }
+  }
+
+  temp = true;
+
+  while (temp) {
+    if (!(sinif === null || sinif === "")) {
+      temp = false;
+    } else {
+      hataliAppear();
+      return false;
+    }
+  }
+
+  temp = true;
+
+  while (temp) {
+    if (!(ogretici === null || ogretici === "")) {
+      temp = false;
+    } else {
+      hataliAppear();
+      return false;
+    }
+  }
+  return true;
 }
--- OgrenciListesiScript.js
+++ OgrenciListesiScript.js
@@ -1,19 +1,37 @@
 window.onload = function () {
+  createArray();
   modalButtonProperties();
   assignmentModalButtonProperties();
   loadTableContents();
   checkTableData();
+  killButtonDetector();
+  buttonDetector();
 };
 
 function checkTableData() {
   const table = document.querySelector(".container");
   const noDataMessage = document.querySelector(".noData");
+  const dbutton = document.getElementById("delete");
+  const abutton = document.getElementById("a-open");
+  const rbutton = document.getElementById("reset");
   if (table.rows.length === 1) {
     noDataMessage.style.display = "block";
     table.style.display = "none";
+    dbutton.classList.add("disabled");
+    dbutton.disable = true;
+    abutton.classList.add("disabled");
+    abutton.disable = true;
+    rbutton.classList.add("disabled");
+    rbutton.disable = true;
   } else {
     noDataMessage.style.display = "none";
     table.style.display = "table";
+    dbutton.classList.remove("disabled");
+    dbutton.disable = false;
+    abutton.classList.remove("disabled");
+    abutton.disable = false;
+    rbutton.classList.remove("disabled");
+    rbutton.disable = false;
   }
 }
 
@@ -30,6 +48,7 @@ function submit() {
     return;
   }
 
+  saveTableContents();
   createNewRow(container, ad, soyad, tcNo, ogrenciNo);
   clearInputFields();
   modalDisappear(modal, overlay);
@@ -37,14 +56,22 @@ function submit() {
 }
 
 function deleteStudent(studentRow) {
+  const contents = JSON.parse(localStorage.getItem("assignments"));
   studentRow.reverse();
   const container = document.querySelector(".container");
   for (let i = 0; i < studentRow.length; i++) {
     if (studentRow[i] >= 1 && studentRow[i] < container.rows.length) {
       container.deleteRow(studentRow[i]);
+      contents.splice(studentRow[i] - 1, 1);
     }
   }
+
+  const json = JSON.stringify(contents);
+  localStorage.setItem("assignments", json);
+  saveTableContents();
   checkTableData();
+  killButtonDetector();
+  buttonDetector();
 }
 
 function saveTableContents() {
@@ -78,6 +105,67 @@ function loadTableContents() {
   }
 }
 
+function modalLoadTableContents() {
+  const container = document.querySelector(".a-container");
+  const contentsJson = localStorage.getItem("classtableContents");
+  if (contentsJson) {
+    const contents = JSON.parse(contentsJson);
+    contents.forEach((rowContents, rowIndex) => {
+      let row = container.rows[rowIndex];
+      if (!row) {
+        row = container.insertRow(rowIndex);
+      }
+      rowContents.forEach((cellContents, cellIndex) => {
+        let cell = row.cells[cellIndex];
+        if (!cell) {
+          cell = row.insertCell(cellIndex);
+        }
+        cell.innerHTML = cellContents;
+      });
+    });
+  }
+}
+
+function createArray() {
+  let assignments = JSON.parse(localStorage.getItem("assignments"));
+  if (!assignments) {
+    assignments = [];
+    localStorage.setItem("assignments", JSON.stringify(assignments));
+  }
+}
+
+function modalSubmit() {
+  const modal = document.getElementById("a-modal");
+  const overlay = document.getElementById("a-overlay");
+  const table = document.querySelector(".container");
+  const atable = document.querySelector(".a-container");
+  let assignments = JSON.parse(localStorage.getItem("assignments"));
+
+  for (let i = 1; i < table.rows.length; i++) {
+    assignments.push([]);
+    const row = table.rows[i];
+    const checkbox = row.querySelector('input[type="checkbox"]');
+    if (checkbox.checked) {
+      const classes = getClassCheckedPositions();
+      for (let j = 0; j < classes.length; j++) {
+        const arow = atable.rows[classes[j]];
+        const classToAdd = arow.cells[1].innerText;
+        if (!assignments[i - 1].includes(classToAdd)) {
+          assignments[i - 1].push(classToAdd);
+        }
+      }
+    }
+  }
+
+  const json = JSON.stringify(assignments);
+  localStorage.setItem("assignments", json);
+  saveTableContents();
+  clearCheckBoxes();
+  modalDisappear(modal, overlay);
+  killButtonDetector();
+  buttonDetector();
+}
+
 function createNewRow(container, ad, soyad, tcNo, ogrenciNo) {
   const newRow = document.createElement("tr");
   const checkbox = document.createElement("input");
@@ -86,20 +174,31 @@ function createNewRow(container, ad, soyad, tcNo, ogrenciNo) {
   const soyadCell = document.createElement("td");
   const tcNoCell = document.createElement("td");
   const ogrenciNoCell = document.createElement("td");
+  const buttonCell = document.createElement("td");
+  const button = document.createElement("BUTTON");
+  const buttonText = document.createTextNode("Dersler");
+  button.appendChild(buttonText);
+
   checkbox.className = "Checkbox";
   checkbox.type = "checkbox";
   adCell.textContent = ad;
   soyadCell.textContent = soyad;
   tcNoCell.textContent = tcNo;
   ogrenciNoCell.textContent = ogrenciNo;
+  button.className = "ders-button";
   newRow.appendChild(boxCell);
   boxCell.appendChild(checkbox);
   newRow.appendChild(adCell);
   newRow.appendChild(soyadCell);
   newRow.appendChild(tcNoCell);
   newRow.appendChild(ogrenciNoCell);
+  newRow.appendChild(buttonCell);
+  buttonCell.appendChild(button);
 
   container.appendChild(newRow);
+  saveTableContents();
+  killButtonDetector();
+  buttonDetector();
 }
 
 function modalAppear(modal, overlay) {
@@ -144,8 +243,16 @@ function assignmentModalButtonProperties() {
   openBtn.addEventListener("click", () => modalAppear(modal, overlay));
   openBtn.addEventListener("click", () => hataliDisappear());
   openBtn.addEventListener("click", () => clearInputFields());
+  openBtn.addEventListener("click", () => modalLoadTableContents());
   closeBtn.addEventListener("click", () => modalDisappear(modal, overlay));
   closeBtn.addEventListener("click", () => hataliDisappear());
+}
+
+function clearCheckBoxes() {
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = false;
+  });
 }
 
 function clearInputFields() {
@@ -156,8 +263,51 @@ function clearInputFields() {
   });
 }
 
-function getCheckedPositions() {
-  const checkboxes = document.querySelectorAll(".Checkbox");
+function resetClasses() {
+  console.log("haha");
+  let assignments = JSON.parse(localStorage.getItem("assignments"));
+  assignments = [];
+  const json = JSON.stringify(assignments);
+  localStorage.setItem("assignments", json);
+}
+
+function killButtonDetector() {
+  const buttons = document.querySelectorAll(".ders-button");
+
+  buttons.forEach((button) => {
+    let newButton = button.cloneNode(true);
+    button.parentNode.replaceChild(newButton, button);
+  });
+}
+
+function classes(index) {
+  const contents = JSON.parse(localStorage.getItem("assignments"));
+  if (contents && contents[index] && contents[index].length > 0) {
+    alert(contents[index]);
+  } else alert("Hiçbir Ders Atanmadı");
+}
+
+function buttonDetector() {
+  document.querySelectorAll(".ders-button").forEach((button, index) => {
+    button.addEventListener("click", () => {
+      classes(index);
+    });
+  });
+}
+
+function getStudentCheckedPositions() {
+  const checkboxes = document.querySelectorAll(".container .Checkbox");
+  const checkedPositions = [];
+  checkboxes.forEach((checkbox, index) => {
+    if (checkbox.checked) {
+      checkedPositions.push(index + 1);
+    }
+  });
+  return checkedPositions;
+}
+
+function getClassCheckedPositions() {
+  const checkboxes = document.querySelectorAll(".a-container .Checkbox");
   const checkedPositions = [];
   checkboxes.forEach((checkbox, index) => {
     if (checkbox.checked) {
@@ -168,11 +318,13 @@ function getCheckedPositions() {
 }
 
 function credentialsCheck(ad, soyad, tcNo, ogrenciNo) {
+  var element = document.querySelector(".hatali");
   let temp = true;
   while (temp) {
     if (!(ad === null || ad === "")) {
       temp = false;
     } else {
+      element.innerText = "Adınızı Yanlış Girdiniz!";
       hataliAppear();
       return false;
     }
@@ -183,6 +335,7 @@ function credentialsCheck(ad, soyad, tcNo, ogrenciNo) {
     if (!(soyad === null || soyad === "")) {
       temp = false;
     } else {
+      element.innerText = "Soyadınızı Yanlış Girdiniz!";
       hataliAppear();
       return false;
     }
@@ -193,6 +346,7 @@ function credentialsCheck(ad, soyad, tcNo, ogrenciNo) {
     if (tcNoCheck(tcNo)) {
       temp = false;
     } else {
+      element.innerText = "T.C. Numaranızı Yanlış Girdiniz!";
       hataliAppear();
       return false;
     }
@@ -200,9 +354,10 @@ function credentialsCheck(ad, soyad, tcNo, ogrenciNo) {
 
   temp = true;
   while (temp) {
-    if (Math.floor(Math.log10(ogrenciNo)) + 1 == 6) {
+    if (ogrenciNo.length === 6) {
       temp = false;
     } else {
+      element.innerText = "Öğrenci Numaranızı Yanlış Girdiniz!";
       hataliAppear();
       return false;
     }
